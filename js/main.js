6f5354489720bc84050e9edc0c6cd0740384a3e1
Leonov-nic <leonov-nic@yandex.ru>
2020-06-18 16:51:05 +0300

    функция OffersOptions

--- js/main.js
+++ js/main.js
@@ -9,97 +9,120 @@ var mapPinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 
-var OffersOptions = {
+var getRandomFromInterval = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-  AUTHOR: {
-    AVATAR: 'img/avatars/user0'NUMBERFOTO'.png'
+var getRandElement = function (array) {
+  var item = array[Math.floor(Math.random() * array.length)];
+  return item;
+};
+
+var getRandomArray = function (array) {
+  var copyArray = array.slice(0, getRandomFromInterval(1, array.length));
+  return copyArray;
+};
+
+var getAvatar = function (min, max) {
+  return 'img/avatars/user0'+ getRandomFromInterval(min, max) + '.png';
+};
+
+var getLocation = function (min, max, ymin, ymax) {
+  return getRandomFromInterval(min, max) + ', ' + getRandomFromInterval(ymin, ymax);
+};
+
+var locationMinMax = {
+  x: {
+    min: 100,
+    max: 800
   },
+  y: {
+    min: 130,
+    max: 630
+  }
+};
 
-  OFFER: {
-    TITLE: [
-      'Крутая хата',
-      'Так себе уголок',
-      'Уютное гнездышко',
-      'Шикарное местечко',
-      'Зачахлый домик',
-      'Дешево и сердито',
-      'Атасный вид',
-      'Это не серьезно'
-    ],
-    ADDRESS: {
-      LOCATION.X: "",
-      LOCATION.Y: ""
-    },
-    PRICE: {
-      MIN: 1000,
-      MAX: 10000
-    },
-    TYPE: [
+var TITLES = [
+  'Крутая хата',
+  'Так себе уголок',
+  'Уютное гнездышко',
+  'Шикарное местечко',
+  'Зачахлый домик',
+  'Дешево и сердито',
+  'Атасный вид',
+  'Это не серьезно'
+];
+
+var TYPES = [
       'palace',
       'flat',
       'house',
       'bungalo'
-    ],
-    ROOMS: {
-    MIN: 1,
-    MAX: 4
-    },
-    GUESTS: {
-      MIN: 1,
-      MAX: 8
-    },
-    CHECKIN: [
+    ];
+var CHECKIN = [
       '12:00',
       '13:00',
       '14:00'
-    ],
-    CHECKOUT: [
+    ];
+
+var CHECKOUT = [
       '12:00',
       '13:00',
       '14:00'
-    ],
-    FEATURES: [
+    ];
+
+var FEATURES = [
       'wifi',
       'dishwasher',
       'parking',
       'washer',
       'elevator',
       'conditioner'
-    ],
-    DESCRIPTION: [
+    ];
+var DESCRIPTIONS = [
       'офигенный',
       'охрененный',
       'просто улёт'
-    ],
-    PHOTOS: [
+    ];
+var PHOTOS = [
       'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
       'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
       'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-    ]
-  },
+    ];
 
-  LOCATION: {
-    X: {
-      MIN: 100,
-      MAX: 800
+var PIN_SIZE = {
+  width: 50,
+  height: 70
+};
+
+var getOffersOptions = function () {
+
+  return {
+
+    author: {
+      avatar: getAvatar(1, 8)
     },
-    Y: {
-      MIN: 130,
-      MAX: 630
-    }
-  },
 
-  NUMBERFOTO: {
-    MIN: 1,
-    MAX: 8
+    offer: {
+      title: getRandomArray(TITLES),
+      address: getLocation(locationMinMax.x.min, locationMinMax.x.max, locationMinMax.y.min, locationMinMax.y.min),
+      price: getRandomFromInterval(1000, 10000),
+      type: getRandomArray(TYPES),
+      rooms: getRandomFromInterval(1, 4),
+      guests: getRandomFromInterval(1, 8),
+      checkin: getRandElement(CHECKIN),
+      checkout: getRandElement(CHECKOUT),
+      feature: getRandomArray(FEATURES),
+      description: getRandElement(DESCRIPTIONS),
+      photo: getRandomArray(PHOTOS)
+    },
+
+    location: {
+      x: getRandomFromInterval(300, 900),
+      y: getRandomFromInterval(130, 630)
+    }
   }
 };
 
-var PIN_SIZE = {
-  WIDTH: 50,
-  HEIGHT: 70
-};
 
-var getRandomFromInterval = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+console.log(getOffersOptions());
